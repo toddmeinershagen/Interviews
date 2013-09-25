29784264a1303ae8c1fbd733e4f9350fc42c741c
Todd Meinershagen <todd@meinershagen.net>
2013-09-25 14:49:56 -0500

    Initial commit

---
+++ Interviews.Rustin/FizzBuzzProcessorTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace Interviews.Rustin
+{
+    [TestFixture]
+    public class FizzBuzzProcessorTests
+    {
+        [Test]
+        public void given_multiple_of_three_return_fizz()
+        {
+            const string expectedValue = "fizz";
+
+            var processor = new FizzBuzzProcessor();
+            var valueFor3 = processor.Process(3);
+            valueFor3.Should.Be(expectedValue);
+            
+            var valueFor12 = processor.Process(12);
+            valueFor12.Should.Be(expectedValue);
+        }
+    }
+}
